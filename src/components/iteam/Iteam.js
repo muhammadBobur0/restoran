e593a75cc54877f3dd982b13a31640f5a72f2347
muhammadBobur0 <muhammadboburbek@gmail.com>
2022-08-18 03:24:08 +0500

    fixed

--- src/components/iteam/Iteam.js
+++ src/components/iteam/Iteam.js
@@ -1,29 +1,33 @@
 import './iteam.css';
 import { useOrder } from '../../hook/useOrder';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 export const Iteam = (props) => {
-	const { bowls, image, name, price } = props.data;
+	const { id, bowls, image, name, price } = props.data;
 	const { data } = props;
 	const { order, setorder } = useOrder();
 
-	// const [click, setClick] = useState([]);
+	const [click, setClick] = useState([]);
 
-	// const handeIteam = () => {
-	// 	const finded = click.findIndex((elemnt) => +elemnt.data.id === +id);
+	const handeIteam = () => {
+		const finded = click.findIndex((elemnt) => +elemnt.data.id === +id);
 
-	// 	if (finded === -1) {
-	// 		setClick([...click, { data, count: 1 }]);
-	// 		console.log(click);
-	// 	} else {
-	// 		click[finded].count += 1
-	// click[finded].price * 2
-	// 	}
-	// };
+		if (finded >= 0) {
+			click[finded].count += 1;
+			click[finded].price = click[finded].price * 2;
+		} else {
+			setClick([...click, { data, count: 1 }]);
+		}
+
+		setorder([...order, { data, count: 1 }]);
+	};
 
 	return (
 		<div
-			onClick={() => setorder([...order, { data, count: 1 }])}
+			onClick={() =>
+				//  setorder([...order, { data, count: 1 }])
+				handeIteam()
+			}
 			className='card'>
 			<img
 				className='food-images'
